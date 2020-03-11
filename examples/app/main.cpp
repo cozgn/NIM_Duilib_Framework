1145003934c029d2f35b4527789d1fa03c401ffa
zocpen <pengzoc@163.com>
2020-03-12 00:05:53 +0800

    update

--- examples/app/main.cpp
+++ examples/app/main.cpp
@@ -38,7 +38,7 @@ void MainThread::Init()
 	// 默认皮肤使用 resources\\themes\\default
 	// 默认语言使用 resources\\lang\\zh_CN
 	// 如需修改请指定 Startup 最后两个参数
-	ui::GlobalManager::Startup(theme_dir + L"resources\\", ui::CreateControlCallback(), false);
+	ui::GlobalManager::Startup(theme_dir + L"resources\\", ui::CreateControlCallback(), true);
 #else
 	// Release 模式下使用资源中的压缩包作为资源
 	// 资源被导入到资源列表分类为 THEME，资源名称为 IDR_THEME
@@ -46,7 +46,7 @@ void MainThread::Init()
 	// 可以使用 OpenResZip 另一个重载函数打开本地的资源压缩包
 	ui::GlobalManager::OpenResZip(MAKEINTRESOURCE(IDR_THEME), L"THEME", "");
 	// ui::GlobalManager::OpenResZip(L"resources.zip", "");
-	ui::GlobalManager::Startup(L"resources\\", ui::CreateControlCallback(), false);
+	ui::GlobalManager::Startup(L"resources\\", ui::CreateControlCallback(), true);
 #endif
 
 	// 创建一个默认带有阴影的居中窗口
